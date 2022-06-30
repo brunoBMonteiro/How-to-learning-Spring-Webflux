d620e40bc9e34b31a189816db07329685d857971
brunoBMonteiro <brunobilheri@hotmail.com>
2022-06-30 14:21:39 -0300

    Create project

---
+++ src/main/java/com/example/learningwebflux/LearningwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningwebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningwebfluxApplication.class, args);
+    }
+
+}
